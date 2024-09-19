8958b1ea25b0cb1eeb6110dd60c9b7b604bf0312
robertrahardja <robertrahardja@gmail.com>
2024-09-19 14:46:32 +0800

    initial commit

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,23 @@
 import { awscdk } from 'projen';
+import { NodePackageManager } from 'projen/lib/javascript';
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'emailgocdk',
+  packageManager: NodePackageManager.NPM,
   projenrcTs: true,
 
-  // deps: [],                /* Runtime dependencies of this module. */
+  deps: [
+    'aws-lambda',
+    'constructs',
+  ],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
+project.addTask('build:lambda', {
+  exec: 'cd lambda && GOOS=linux GOARCH=amd64 go build -o main main.go',
+});
+
 project.synth();
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,33 @@
 import { App, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as cdk from 'aws-cdk-lib';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
     // define resources here...
+    const goFunction = new lambda.Function(this, 'GoFunction', {
+
+      runtime: lambda.Runtime.GO_1_X,
+      handler: 'main',
+      code: lambda.Code.fromAsset('lambda'),
+
+    })
+
+
+    const functionUrl = goFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ['*'],
+      }
+    })
+
+    new cdk.CfnOutput(this, 'GoFunctionUrl', {
+      value: functionUrl.url,
+      description: 'The URL of the Go function'
+    })
   }
 }
 
